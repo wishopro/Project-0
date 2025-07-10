922e0eef783708b5cb528d54f04af5673b409092
luis <wishotrutie@gmail.com>
2025-07-10 18:39:54 -0400

    Fixed bug and now deletes index.html title

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ function loadSection(section) {
     let path = '';
 
     if (section === 'main') {
-        location.reload(); // Simple way to reset to main
+        location.reload();
         return;
     } else if (section === 'images') {
         path = 'images.html';
     } else if (section === 'advanced') {
         path = 'advanced.html';
     }
+
+    // Clean up existing h1 before injecting new content
+    const oldHeader = document.querySelector('h1.imageslogo');
+    if (oldHeader) oldHeader.remove();
 
     fetch(path)
         .then(res => res.text())
